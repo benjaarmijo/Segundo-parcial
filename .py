8c6e7f934d70584d284bc74a547a1e0e08a4381d
benjaarmijo <be.armijor@duocuc.cl>
2026-05-11 10:50:16 -0400

    Add files via upload

--- .py
+++ .py
@@ -1,21 +1,22 @@
-# 1. desarrolle un programa en python que permita calcular el valor final de un bicicletero (estacionamiento para motos),
-#  y del candado para el centro civico que ofrece descueto para el guardado de su moto mensual
-# segun los dias de uso. valor inicial: motocicleta 15.000 pesos mensuales, candado: 9.000 mensuales. realize una tabla donde va a tener dias de uso, estudiante, y descuento de la motocicleta
-# cantidad de dias >= 20 para estudiante debera usar la palabra si y tendra un 25% de descuento
-# cantidad de dias >= 20 a los no estudiantes y descuento del 15% 
-# cantidad de dias 10 <=20  si estudiantes descuento 15% 
-# cantidad de dias >=20 no estudiantes descuento 8%
-# ademas el candado tiene una regla adicional los estudiantes obtienes un 12% adicional por pago por tarjeta de credito
-# si ademas la cantidad de dias es menor a 15 obtendras solo un 15%
-# si la cantidad de dias es menor a 10 el descuento es de 0
+suma = 0
 
-# 2. desarrolle un programa que permita ingresar dos numeros enteros que indique un rango de numeros el primero debe ser menor al segundo
-# luego el programa debe generar un numero aleatorio entre el rango de los numeros, para hacer esto sigue las siguientes intrucciones
-# From random import randint
-# la linea 1 le permite cargar la funcion randint, luego la linea 2 se va a usar la funcion randint que va a generar un numero aleatorio entre los dos valores
-# a modo de ejemplo: randint (1,10). genero un numero aleatorio entre el 1 y 10.
-# un generador de numero aleatorio debe ajustar el numero para que el valor final sea adivinado por el usuario luego de 3 intentos 
-# si el numero ajustado llega a quedar fuera del rango, entonces el numero debe dividirse por le limite inferior 
+for i in range (5):
+    
+    while True:
+        try:
+            numero = float(input(f"Ingrese el numero {i + 1}: "))
+            
+            suma = suma + numero
 
+            break
+        except:
 
+            print("Error, debe ingresar un numero valido")
 
+print("La suma total es: ", suma)            
+
+"""
+Hay un pequeña tienda que desea modernizar su sistema de venta, actualmente el vendedor lleva un modesto registro manual y al final del dia debe calcular todo lo vendido. 
+Para facilitar el trabajo necesitamos crear un programa en python y cuando hagamos la venta le debe solicitar sus datos, nombre, rut, el carnet en general, una vez que tenga todos
+los datos el usuario debe introducir 10 productos para comprar, una vez realizada la compra, debe mostrar el resultado de la compra si tiene alguna descuento muestra el descuento
+"""
